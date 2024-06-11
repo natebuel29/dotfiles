941437df3a83fbded78a844977d10dd4a04e32a6
Nate Buel <natebuel29@gmail.com>
2024-06-10 21:06:29 -0500

    fix script and package list

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-if [ -e /etc/apt/preferences.d/nosnap.pref]
+if [ -e /etc/apt/preferences.d/nosnap.pref ]
 then
     sudo rm /etc/apt/preferences.d/nosnap.pref
 else
@@ -7,16 +7,17 @@ fi
 sudo xargs -a completePackage.txt apt install
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo y | sh ./install.sh
+sh ./install.sh
 rm install.sh
 
-# clone
+# Clone and install Powerline fonts
 git clone https://github.com/powerline/fonts.git --depth=1
-# install
 cd fonts
 ./install.sh
-# clean-up a bit
 cd ..
 rm -rf fonts
 
+# Install Zsh autosuggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+echo "Script executed successfully"
